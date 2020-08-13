114c66726cc529baa365a7c6221d1e249ea1210b
Seda Shahgyaldyan <seda_sha@yahoo.com>
2020-08-13 01:23:07 -0400

    js karch

--- calc.js
+++ calc.js
@@ -16,13 +16,11 @@
     let getResultYear = document.querySelector(".result_year")
     let finalResult = document.querySelector(".final_result");
     ////------------------calculate-1---------------//
-    
     let startMoney = document.querySelector(".start_money");
     let divYield = document.querySelector(".div_yield");
     let divGrowth = document.querySelector(".div_growth");
     let taxes = document.querySelector(".tax");
     let years = document.querySelector(".years");
-    let payTime = document.querySelector(".pay_time");
     let annually = document.querySelector(".ann");
     let quartely = document.querySelector(".quar");
     let monthly = document.querySelector(".month");
@@ -32,27 +30,14 @@
     
     let finalText = document.createElement("p");
     let dividend, money1, checking = true, divi, lift, moneyFormatted, dividendFormatted;
-  
+    divi = document.createElement("div");
+    divi.setAttribute("class", "resultTver")
+    result.appendChild(divi);
+    // ------reload-----------//
+    newStart.addEventListener("click", ()=>{
+        location.reload()
+     });
     ////------------------calculate-1---------------//
-    function divedent(money,yield,growth,tax){
-        return (((money/100 * yield) + ((money / 100 * yield) / 100 * growth))-(((money/100 * yield) + ((money / 100 * yield) / 100 * growth)) / 100 * tax))
-    }
-    function calc1main(m) {
-        moneyFormatted = money1.toFixed(2);
-        dividendFormatted = dividend.toFixed(2);
-        divi.style.display = "inline";
-        let resultyear = document.createElement("span");
-        resultyear.innerHTML = `
-        <span>${m}</span><span>${dividendFormatted}</span> <span>${moneyFormatted}</span>
-        <br>`;
-        divi.appendChild(resultyear);
-        resultyear.style = `
-        padding-left: 30px;
-        box-sizing:border-box;  
-        display:flex;
-        flex-direction:row;
-        justify-content: space-around;`;
-    }
     but1.addEventListener("click", ()=>{
         if (checking == false){ 
             divi.remove()
@@ -65,42 +50,22 @@
         let growth = +divGrowth1.value;
         let tax = +taxes1.value;
         let m = 1;
-        divi = document.createElement("div");
-        divi.setAttribute("class", "resultTver")
-        result.appendChild(divi);
         getResultYear.innerHTML = `Years`;
         money1 = money;
         if (startMoney1.value != "" && divYield1.value != ""){
            for (let i = 0; i < +years1.value; i++){
                dividend = divedent(money,yield,growth,tax)
-            money1 +=  dividend;
-            calc1main(m)
+            money1 += dividend;
+            moneyFormatted = money1.toFixed(2);
+            dividendFormatted = dividend.toFixed(2);
+            calc(m)
             m++
-        }
+            }
         result.style.display = 'block';
         emptyValue(startMoney1, years1, startMoney, divYield, divGrowth, taxes, years);
-    }
-       
+        }   
     });
-    ////------------------calculate-1------------------------------------------------------//
-    
-    function calc1mainDrip(m) {
-        moneyFormatted = money.toFixed(2);
-        dividendFormatted = dividend.toFixed(2);
-        divi.style.display = "inline";
-        let resultyear = document.createElement("span");
-        resultyear.innerHTML = `
-        <span>${m}</span><span>${dividendFormatted}</span> <span>${moneyFormatted}</span>
-        <br>`;
-        divi.appendChild(resultyear);
-        resultyear.style = `
-        padding-left: 30px;
-        box-sizing:border-box;  
-        display:flex;
-        flex-direction:row;
-        justify-content: space-around;`;
-    }
-   
+    ////------------------calculate-Drip------------------------------------------------------//
     but.addEventListener("click", ()=>{
         if (checking == false){ 
             divi.remove()
@@ -108,28 +73,23 @@
             getResultYear.innerHTML = ``;
         }
         checking = false;
-        
         aler(startMoney, divYield, years);
         let money = +startMoney.value;
         let yield = +divYield.value; 
         let growth = +divGrowth.value;
         let tax = +taxes.value;
         let m = 1;
-        divi = document.createElement("div");
-        divi.setAttribute("class", "resultTver")
-        result.appendChild(divi);
-        
         if (getSel.value == "ann" && startMoney.value != "" && divYield.value != ""){
             getResultYear.innerHTML = `Years`;
             for (let i = 0; i < +years.value; i++){
                 dividend = divedent(money,yield,growth,tax)
                 money += dividend;
-                calc1mainDrip(m)
-               
+                moneyFormatted = money.toFixed(2);
+                dividendFormatted = dividend.toFixed(2);
+                calc(m)
                 m++
             };
-            result.style.display = 'block';
-            emptyValue(startMoney, years,  startMoney1, divYield1, divGrowth1, taxes1, years1);
+          
         }
         if (getSel.value == "month" && startMoney.value != "" && divYield.value != ""){
             getResultYear.innerHTML = `Months`;
@@ -137,63 +97,37 @@
             yield = yield / 12;
             growth = growth / 12;
             for (let j = 0; j < yearsM; j++){
-                    dividend = (((money/100 * yield) + (money / 100 * yield) / 100 * growth)-( ((money/100 * yield) + (money / 100 * yield) / 100 * growth) / 100 * tax))
+                dividend = divedent(money,yield,growth,tax)
                 money = money + dividend;
                 moneyFormatted = money.toFixed(2);
                 dividendFormatted = dividend.toFixed(2);
-                divi.style.display = "inline";
-                let resultyear = document.createElement("span");
-                resultyear.innerHTML = `
-                <span>${m}</span><span>${dividendFormatted}</span> <span>${moneyFormatted}</span>
-                <br>`;
-                divi.appendChild(resultyear);
-                resultyear.style = `
-                    padding-left: 30px;
-                    box-sizing:border-box;
-                    display:flex;
-                    flex-direction:row;
-                    justify-content: space-around;`;
+               calc(m)
                 m++
             }
-            result.style.display = 'block';
-            emptyValue(startMoney, years,  startMoney1, divYield1, divGrowth1, taxes1, years1);
         }
-            if (getSel.value == "quar" && startMoney.value != "" && divYield.value != ""){
-                getResultYear.innerHTML = `Quarters`;
-                 let yearsQ = +years.value * 4;
-                 yield = yield / 4;
-                 growth = growth / 4;
-                 for (let j = 0; j < yearsQ; j++){
-                         dividend = (((money/100 * yield) + (money / 100 * yield) / 100 * growth)-( ((money/100 * yield) + (money / 100 * yield) / 100 * growth) / 100 * tax))
-                     money = money + dividend;
- 
-                     moneyFormatted = money.toFixed(2);
-                     dividendFormatted = dividend.toFixed(2);
-                     divi.style.display = "inline";
-                     let resultyear = document.createElement("span");
-                     resultyear.innerHTML = `
-                     <span>${m}</span><span>${dividendFormatted}</span> <span>${moneyFormatted}</span>
-                     <br>`;
-                     divi.appendChild(resultyear);
-                     resultyear.style = `
-                        padding-left: 30px;
-                        box-sizing:border-box;
-                         display:flex;
-                         flex-direction:row;
-                         justify-content: space-around;`;
-                     m++
-                 }
-                 result.style.display = 'block';
-                 emptyValue(startMoney, years,  startMoney1, divYield1, divGrowth1, taxes1, years1);
-            }
+        if (getSel.value == "quar" && startMoney.value != "" && divYield.value != ""){
+            getResultYear.innerHTML = `Quarters`;
+             let yearsQ = +years.value * 4;
+             yield = yield / 4;
+             growth = growth / 4;
+             for (let j = 0; j < yearsQ; j++){
+                dividend = divedent(money,yield,growth,tax);
+                 money += dividend;
+                 moneyFormatted = money.toFixed(2);
+                 dividendFormatted = dividend.toFixed(2);
+                  calc(m)
+                 m++
+             }
+        }
+            result.style.display = 'block';
+            emptyValue(startMoney, years, startMoney1, divYield1, divGrowth1, taxes1, years1);
     });
     lift = document.createElement("a");
     lift.setAttribute("id", "pop");
     result.appendChild(lift);
 
-    newStart.addEventListener("click", ()=>{
-       location.reload()
-    });
+    
+// ------------------Responsive------------------///
     simple.addEventListener("click", ()=>{
         simpleCalc.style.display = 'block';
         usefuld.style.display = 'block';
@@ -208,7 +142,7 @@
         drip.style.display = 'none';
         note.style.display = 'block';
     });
-
+// /----------------functions---------------//
     function aler(a, b, c) {
         if (a.value == "" ){
             alert("Please input the Starting Principal");
@@ -229,9 +163,22 @@
         taxes1.value = "";
         years1.value = "";
     }
-  
-
-
+    function divedent(money,yield,growth,tax){
+        return (((money/100 * yield) + ((money / 100 * yield) / 100 * growth))-(((money/100 * yield) + ((money / 100 * yield) / 100 * growth)) / 100 * tax))
+    }
+    function calc(m){
+        divi.style.display = "inline";
+        let resultyear = document.createElement("span");
+        resultyear.innerHTML = `
+        <span>${m}</span><span>${dividendFormatted}</span> <span>${moneyFormatted}</span> <br>`;
+        divi.appendChild(resultyear);
+        resultyear.style = `
+        padding-left: 30px;
+        box-sizing:border-box;  
+        display:flex;
+        flex-direction:row;
+        justify-content: space-around;`;
+    }
     
   
 
